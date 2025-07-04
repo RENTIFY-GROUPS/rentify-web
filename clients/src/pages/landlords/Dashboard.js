5ff6c7014fb5207ca669f7e19ef66f91357f9b30
Daboggieman <adaraph733@gmail.com>
2025-07-04 18:51:37 +0100

    frontend deployment fixes, gemini 14

--- clients/src/pages/landlords/Dashboard.js
+++ clients/src/pages/landlords/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../utils/api';
-import PropertyCard from '../components/PropertyCard';
+import PropertyCard from '../../components/PropertyCard';
 import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
 
